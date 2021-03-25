eef29f3234a9593d74ba80be277171899b11b50f
John Limkeman <john.limkeman@gmail.com>
2021-03-25 17:09:07 -0500

    pushing

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="app">
+    <img id="logo" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg" alt="The Movie Database">
+   <br/>
     <select name="typeToggle"  v-model="query.type">
     <option value="all">All</option>
     <option value="movie">Movies</option>
     <option value="tv">TV Shows</option>
     <option value="person">People</option>
     </select>
-    <input type="text" v-model="query.text">
+    <input type="text" v-model="query.text" >
     <button v-on:click="conductSearch()">SEARCH</button>
-    <ResultView v-bind:results="results"/>
+    <ResultView v-bind:config="api.configuration" v-bind:results="results" v-if="results.length > 0"/>
   </div>
 </template>
 
 <script>
-
-
+import axios from 'axios'
+import ResultView from './components/ResultView.vue'
 export default {
   name: 'App',
   data(){
@@ -27,9 +29,11 @@ export default {
           type: "movie",
         },
         api: {
-          base: "",
-          key: "",
-          params: "&language=en-US&include_adult=false&query="
+          base: "https://api.themoviedb.org/3",
+          key: '917369bb118862e487faedc2fa6bf48d',
+          params: "&language=en-US&include_adult=false&query=",
+          page: "&page=",
+          configuration: {},
         }
     }
   },
@@ -46,18 +50,50 @@ export default {
     },
     searchMovies(){
       console.log("movie search...")
-
+      try{
+        axios.get(`${this.api.base}/search/movie?api_key=${this.api.key}${this.api.params}${this.query.text}${this.api.page}${this.query.page}`)
+        .then(result =>{
+          this.results = result.data.results;
+        })
+        console.log(this.results)
+      } catch(error){
+        console.log("error")
+      }
     },
     searchTV(){
       console.log("tv search...")
+       try{
+        axios.get(`${this.api.base}/search/tv?api_key=${this.api.key}${this.api.params}${this.query.text}${this.api.page}${this.query.page}`)
+        .then(result =>{
+          this.results = result.data.results;
+        })
+        console.log(this.results)
+      } catch(error){
+        console.log("error")
+      }
     },
     searchPeople(){
       console.log("people search...")
+       try{
+        axios.get(`${this.api.base}/search/person?api_key=${this.api.key}${this.api.params}${this.query.text}${this.api.page}${this.query.page}`)
+        .then(result =>{
+          this.results = result.data.results;
+        })
+        console.log(this.results)
+      } catch(error){
+        console.log("error")
+      }
     }
 
   },
   components: {
-
+    ResultView
+  },
+  mounted() {
+    
+      axios.get(`${this.api.base}/configuration?api_key=${this.api.key}`).then(response => {
+        this.api.configuration = response.data;
+      })
   }
 }
 </script>
@@ -70,5 +106,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
 }
+  #logo{
+height: 200px;
+width: auto;
+  }
 </style>
---
+++ src/components/ResultView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h1>RESULTS</h1>
+      <div v-for="result in results" v-bind:key="result.id">
+          <img src="" alt="">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+data(){
+    return{
+
+    }
+},
+props: ["results", "config"]
+}
+</script>
+
+<style>
+
+</style>
